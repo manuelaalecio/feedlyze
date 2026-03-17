f72ce3eebb067d0732fa473f1bdd5deba902a7a2
Manuela Alecio <manuela@codifica.dev>
2026-03-17 17:02:59 -0300

    feat: integrate Clerk for authentication and enhance layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+
+import { Toaster } from "@/components/ui/sonner";
+import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -17,10 +22,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" suppressHydrationWarning>
-      <body className={cn("font-sans antialiased", inter.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="pt-BR" suppressHydrationWarning>
+        <body
+          className={cn(
+            "font-sans antialiased min-h-screen flex flex-col",
+            inter.variable,
+          )}
+        >
+          {/* Navbar */}
+          <Navbar />
+          {/* Main Section */}
+          <main className="flex-1 container mx-auto px-4 py-8">{children}</main>
+          {/* Footer */}
+          <Footer />
+          <Toaster />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
